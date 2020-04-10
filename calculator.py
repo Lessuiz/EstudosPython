d2271c299121cb5d1a97474ae24182658ef1d5e2
Lessuiz <leonardoparreira2018@gmail.com>
2020-04-10 15:49:06 -0300

    Criei calculator.py

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def somar(x, y):
+    return x+y
+def subtrair(x, y):
+    return x-y
+def multiplicar(x, y):
+    return x*y
+def dividir(x, y):
+    return x/y
+def elevar(x, y):
+    return x**y
+
+print(elevar(2,4))
